9f826fbd56461549742cee4e66dc52dfe3d34813
Brandon Wetzel <mauzybwy@gmail.com>
2025-12-22 20:30:41 -0500

    Dir locals

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+(((lua-ts-mode pico8-mode)
+  (line-spacing . 4)
+  (tab-width . 2)
+  (eval
+   (lambda ()
+     (auto-fill-mode)
+     (set-fill-column 50)
+     (face-remap-add-relative 'default '(:family "PICO-8" :weight regular :height 180))))))
